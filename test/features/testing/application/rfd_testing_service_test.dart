1a0664e1a31cac3fef71061784db1612d89e5f26
akladnig <adrian@kladnig.com>
2023-10-17 10:55:23 +1100

    First commit

---
+++ test/features/testing/application/rfd_testing_service_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_tindeq/src/features/testing/application/rfd_testing_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../repository/test_data.dart';
+
+void main() {
+  // _lineParameters
+  test('lineParameters', () {
+    expect(lineParameters((-2, 6), (5, 27)), (3, 12));
+  });
+  // rfdPeak
+  test('rfdPeak', () {
+    expect(rfdWave.rfdResult, (
+      peak: 169.561733525,
+      peakPoint: (0.0, 10.0),
+      peakLine: (
+        (-0.04413592728183971, 1.931986301),
+        (0.04041524981991993, 17.387876709)
+      ),
+      mean: 190.4592246,
+      timeToPeak: 0.0
+    ));
+  });
+}
