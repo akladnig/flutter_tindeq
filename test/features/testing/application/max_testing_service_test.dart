05cbd61b086d87154a5edccf92539d96db173885
akladnig <adrian@kladnig.com>
2023-10-17 11:24:25 +1100

    Rationalised testing for 100% coverage

--- test/features/testing/application/max_testing_service_test.dart
+++ test/features/testing/application/max_testing_service_test.dart
@@ -1,20 +1,14 @@
-import 'package:flutter_tindeq/src/features/testing/application/max_testing_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../repository/test_data.dart';
 
 void main() {
-  // mean
-  test('mean', () {
-    expect(squareWave.mean, (force: 17.966490519090907, time: 17.55128099));
-  });
-
-  // meanLine
-  test('meanLine', () {
-    expect(squareWave.meanLine,
-        ((0.0, 17.966490519090907), (0.5, 17.966490519090907)));
-  });
-
   // maxResult
-  test('maxResult', () {});
+  test('maxResult', () {
+    expect(squareWave.maxResult, (
+      maxStrength: 19.31986301,
+      meanStrength: 17.966490519090907,
+      meanLine: ((0.0, 17.966490519090907), (0.5, 17.966490519090907)),
+    ));
+  });
 }
