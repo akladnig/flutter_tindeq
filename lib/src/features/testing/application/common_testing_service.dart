5b54f3390b35f7e26d57517fff52588be08f1096
akladnig <adrian@kladnig.com>
2023-10-20 19:05:51 +1100

    Added method to get rfd from a list of edges and convert index to point

--- lib/src/features/testing/application/common_testing_service.dart
+++ lib/src/features/testing/application/common_testing_service.dart
@@ -3,11 +3,6 @@ import 'package:flutter_tindeq/src/features/testing/application/max_testing_serv
 import 'package:flutter_tindeq/src/features/testing/domain/testing_models.dart';
 import 'package:statistics/statistics.dart';
 
-enum Sequence { increment, decrement }
-
-/// Returns a sub list of List<double> of either force or time between the start and end values.
-//TODO check that end >= start
-
 extension CommonTestingService on PointListClass {
 // class CommonTestingService extends PointListClass {
 //   CommonTestingService(super.pointList);
@@ -71,19 +66,50 @@ extension CommonTestingService on PointListClass {
     return index;
   }
 
-  bool _diffCondition(edge, diff, currentEdge) {
-    return edge == EdgeType.rising ? diff > currentEdge : diff < currentEdge;
+  /// Calculates the RFDs from either a list of rising or falling points
+  List<int> rfdIndexList(EdgeType edgeType) {
+    int index = 0;
+    List<int> indexList = [];
+
+    if (edgeList(edgeType).isNotEmpty) {
+      for (var edge in edgeList(edgeType)) {
+        PointList edgePoints = sublist(edge.$1, edge.$2 + 1);
+
+        double diff = 0.0;
+        double currentEdge = 0.0;
+        index = 0;
+        for (var i = sampleInterval;
+            i < edgePoints.length - sampleInterval;
+            i++) {
+          int j = i - sampleInterval;
+          int k = i + sampleInterval;
+          diff = edgePoints[k].$2 - edgePoints[j].$2;
+          if (_diffCondition(edgeType, diff, currentEdge)) {
+            index = i;
+            currentEdge = diff;
+          }
+        }
+        index += edge.$1;
+        indexList.add(index);
+      }
+    }
+    return indexList;
   }
 
-  List<(int, int)> edgeList(EdgeType edge) {
-    // return a list of the indexes of rising points that have more than 5 sequential points
+  // return a list of the indexes of rising points that have more than 5 sequential points
+  // and lie between the trigger Level and 90% of max Strength
+  // also discard any lists that have a diff of less than 3kg
+  // TODO using 3kg for now but this should probably be increased -
+  // I mean what is the minimum force anyone can pull? or perhaps base this on min RFD?
+  List<(int, int)> edgeList(EdgeType edge, [int? offset]) {
     var list = <(int, int)>[];
-    // const int minLength = sampleInterval * 2 + 1;
 
     const int minLength = sampleInterval * 2 + 1;
     int currentLength = 1;
     int startIndex = 0;
     int endIndex = 0;
+
+    offset = offset ?? 0;
 
     for (var i = 0; i < pointList.length - 1; i++) {
       // if the points are increasing keep bumping the index up
@@ -96,9 +122,9 @@ extension CommonTestingService on PointListClass {
         // when the points stop increasing, check if it satisfies the minimum length and save
         // otherwise reset the indexes
         if (startIndex <= endIndex) {
-          if (currentLength >= minLength) {
-            list.add((startIndex, endIndex));
-            // list.add((startIndex, currentLength));
+          if ((currentLength >= minLength) &&
+              _minDiffCondition(edge, startIndex, endIndex)) {
+            list.add((offset + startIndex, offset + endIndex));
           }
           startIndex = i + 1;
           endIndex = startIndex;
@@ -109,6 +135,11 @@ extension CommonTestingService on PointListClass {
     return list;
   }
 
+  bool _diffCondition(edge, diff, currentEdge) {
+    return edge == EdgeType.rising ? diff > currentEdge : diff < currentEdge;
+  }
+
+  // Checks if the point is on a valid edge - between the trigger level and 90% ofmax strength
   bool isOnEdge(Point point) {
     double maxForce = 0.9 * maxStrength.force;
     return (triggerLevel <= point.$2) && (point.$2 <= maxForce);
@@ -123,5 +154,29 @@ extension CommonTestingService on PointListClass {
         edgeCondition = (pointList[i + 1].$2 < pointList[i].$2);
     }
     return edgeCondition;
+  }
+
+  bool _minDiffCondition(edge, startIndex, endIndex) {
+    bool edgeCondition = false;
+    switch (edge) {
+      case EdgeType.rising:
+        edgeCondition =
+            (pointList[endIndex].$2 - pointList[startIndex].$2 > triggerLevel);
+      case EdgeType.falling:
+        edgeCondition =
+            (pointList[startIndex].$2 - pointList[endIndex].$2 > triggerLevel);
+    }
+    return edgeCondition;
+  }
+
+  Point toPoint(int index) => pointList[index];
+
+  PointList toPointList(List<int> indexList, [int? startIndex]) {
+    PointList rfdList = [];
+    startIndex = startIndex ?? 0;
+    for (var index in indexList) {
+      rfdList.add(pointList[startIndex + index]);
+    }
+    return rfdList;
   }
 }
