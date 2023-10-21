1b977d4fffef40c594e380eb8d6f3d0867fe8e52
akladnig <adrian@kladnig.com>
2023-10-22 09:23:01 +1100

    rationalising code duplication for getEdge and refactoring PointListClass to PointList

--- lib/src/features/testing/application/common_testing_service.dart
+++ lib/src/features/testing/application/common_testing_service.dart
@@ -3,10 +3,7 @@ import 'package:flutter_tindeq/src/features/testing/application/max_testing_serv
 import 'package:flutter_tindeq/src/features/testing/domain/testing_models.dart';
 import 'package:statistics/statistics.dart';
 
-extension CommonTestingService on PointListClass {
-// class CommonTestingService extends PointListClass {
-//   CommonTestingService(super.pointList);
-
+extension CommonTestingService on PointList {
   List<double> subListByType(
     ListType type, {
     int start = 0,
@@ -38,46 +35,30 @@ extension CommonTestingService on PointListClass {
   /// Get the Minimum and Maximum RFD from a list of data points
   /// Looks for the difference between 5 points which is around 100ms
   /// for a sampling rate of 20ms
-  //TODO retun null if an edge is not found?
-  (int, int) get getEdge =>
-      (getEdgeIndex(EdgeType.rising), getEdgeIndex(EdgeType.falling));
-
-  /// Get the first edge index of either a list of rising or falling points
-  int getEdgeIndex(EdgeType edgeType) {
-    double diff = 0.0;
-    double currentEdge = 0.0;
-    int index = 0;
-
-    if (edgeList(edgeType).isNotEmpty) {
-      (int, int) firstEdge = edgeList(edgeType)[0];
-      PointList edge = sublist(firstEdge.$1, firstEdge.$2 + 1);
-
-      for (var i = sampleInterval; i < edge.length - sampleInterval; i++) {
-        int j = i - sampleInterval;
-        int k = i + sampleInterval;
-        diff = edge[k].$2 - edge[j].$2;
-        if (_diffCondition(edgeType, diff, currentEdge)) {
-          index = i;
-          currentEdge = diff;
-        }
-      }
-      index += firstEdge.$1;
-    }
-    return index;
+  //TODO retun null if an edge is not found? or leave at zero?
+  (int, int) get getEdge {
+    int risingIndex = rfdIndexList(EdgeType.rising).isEmpty
+        ? 0
+        : rfdIndexList(EdgeType.rising)[0];
+    int fallingIndex = rfdIndexList(EdgeType.falling).isEmpty
+        ? 0
+        : rfdIndexList(EdgeType.falling)[0];
+    return (risingIndex, fallingIndex);
   }
 
-  /// Calculates the RFDs from either a list of rising or falling points
+  /// Gets the RFDs from either a list of rising or falling points
   List<int> rfdIndexList(EdgeType edgeType) {
     int index = 0;
     List<int> indexList = [];
 
-    if (edgeList(edgeType).isNotEmpty) {
-      for (var edge in edgeList(edgeType)) {
-        PointList edgePoints = sublist(edge.$1, edge.$2 + 1);
+    if (edgeIndexList(edgeType).isNotEmpty) {
+      for (var edge in edgeIndexList(edgeType)) {
+        Points edgePoints = sublist(edge.$1, edge.$2 + 1);
 
         double diff = 0.0;
         double currentEdge = 0.0;
         index = 0;
+
         for (var i = sampleInterval;
             i < edgePoints.length - sampleInterval;
             i++) {
@@ -101,7 +82,7 @@ extension CommonTestingService on PointListClass {
   // also discard any lists that have a diff of less than 3kg
   // TODO using 3kg for now but this should probably be increased -
   // I mean what is the minimum force anyone can pull? or perhaps base this on min RFD?
-  List<(int, int)> edgeList(EdgeType edge, [int? offset]) {
+  List<(int, int)> edgeIndexList(EdgeType edge, [int? offset]) {
     var list = <(int, int)>[];
 
     const int minLength = sampleInterval * 2 + 1;
@@ -171,8 +152,8 @@ extension CommonTestingService on PointListClass {
 
   Point toPoint(int index) => pointList[index];
 
-  PointList toPointList(List<int> indexList, [int? startIndex]) {
-    PointList rfdList = [];
+  Points toPointList(List<int> indexList, [int? startIndex]) {
+    Points rfdList = [];
     startIndex = startIndex ?? 0;
     for (var index in indexList) {
       rfdList.add(pointList[startIndex + index]);
